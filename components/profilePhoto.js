68f28d0d8f17bc051372f49c936d0e9c8570306b
Tianzi Cui <cuitianzi0@gmail.com>
2024-11-09 22:48:34 -0700

    changed profilePhoto animation to spin

--- components/profilePhoto.js
+++ components/profilePhoto.js
@@ -2,16 +2,15 @@ import Image from "next/image";
 
 function ProfilePhoto() {
   return (
-    <div className="w-1/2 border flex justify-center items-center">
-      <div className="w-[280px] h-[280px] border rounded-full flex justify-center items-center">
-        <div className="w-[270px] h-[270px] border rounded-full relative ">
-          <Image
-            src="/images/lexie.jpg"
-            alt="Lexie"
-            layout="fill"
-            className="rounded-full object-cover animate-fade-down animate-once animate-ease-in"
-          />
-        </div>
+    <div className="w-1/2  flex justify-center items-center">
+      <div className="w-[300px] h-[300px] rounded-full flex justify-center items-center border-[4px] border-dashed border-[#00FF9C] animate-spin animate-infinite animate-duration-[12000ms] animate-linear animate-delay-[3000ms]"></div>
+      <div className="w-[270px] h-[270px] rounded-full absolute animate-fade animate-once animate-duration-[5000ms] animate-ease-linear">
+        <Image
+          src="/images/lexie.jpg"
+          alt="Lexie"
+          layout="fill"
+          className="rounded-full object-cover"
+        />
       </div>
     </div>
   );
